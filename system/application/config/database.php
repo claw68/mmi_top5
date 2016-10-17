bf6593f7b23244768333a4742220fa126075ae3d
claw68 <franzandrew68@gmail.com>
2016-10-18 00:04:03 +0800

    Update password for db

--- system/application/config/database.php
+++ system/application/config/database.php
@@ -39,7 +39,7 @@ $active_record = TRUE;
 
 $db['default']['hostname'] = "localhost";
 $db['default']['username'] = "root";
-$db['default']['password'] = "fs0123456";
+$db['default']['password'] = "root";
 $db['default']['database'] = "top_db";
 $db['default']['dbdriver'] = "mysql";
 $db['default']['dbprefix'] = "";
